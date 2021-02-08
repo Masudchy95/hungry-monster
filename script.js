060025e5a085eb1b7d44e6770f491c13f6d0173c
Masud Chy <masud3e.095@gmail.com>
2021-02-08 20:53:27 +0600

    Another commit

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const mealSearchDisplay = () => {
   const inputSearch = document.getElementById("input-search").value;
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${inputSearch}`
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${inputSearch}`;
   fetch(url)
-  .then(res => res.json())
-  .then(data => mealNameDisplay(data.meals))
-}
+    .then((res) => res.json())
+    .then((data) => mealNameDisplay(data.meals));
+};
 
-const mealNameDisplay = meals => {
+const mealNameDisplay = (meals) => {
   const mealContainer = document.getElementById("meal-container");
-  mealContainer.innerHTML = '';
- meals.forEach (meal => {
-   const mealDiv = document.createElement("div");
-   mealDiv.innerHTML = `
+  mealContainer.innerHTML = "";
+  meals.forEach((meal) => {
+    const mealDiv = document.createElement("div");
+    mealDiv.className = "meal-details";
+    mealDiv.innerHTML = `
         <img src="${meal.strMealThumb}" alt="" width= "250px">
-        <p class="meal-name">${meal.strMeal}</p>
+        <h5 class="meal-name">${meal.strMeal}</h5>
+        <p>${meal.strCategory}</p>
    `;
-   mealContainer.appendChild(mealDiv);
- })
-}
+    mealContainer.appendChild(mealDiv);
+  });
+};
